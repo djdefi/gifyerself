a43e4767e1682b1d48540e203855834170386eeb
Ryan Trauntvein <djdefi@users.noreply.github.com>
2020-05-18 20:03:32 -0700

    Initial commit

---
+++ app.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# app.rb
+require 'sinatra'
+
+dirname = __dir__
+
+def file_names(path)
+  Dir.entries(path).select { |f| File.file? File.join(path, f) }
+end
+
+get '/' do
+  'Hello world!'
+end
+
+get '/*' do
+  path = dirname + '/public' + request.path_info
+  puts "Loading..."
+  puts dirname
+  puts request.path_info
+  puts path
+  erb :slides, locals: { files: file_names(path), path: request.path_info }
+end
